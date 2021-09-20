34e1bce04975664fc4241bb6a21acb882768b324
Brian Vu <emailbrian.vu@gmail.com>
2021-09-20 13:27:46 -0700

    ci: added testing for aws-iam-authenticator

--- src/scripts/install-aws-iam-authenticator.sh
+++ src/scripts/install-aws-iam-authenticator.sh
@@ -4,11 +4,10 @@ if which aws-iam-authenticator > /dev/null; then
     exit 0
 fi
 PLATFORM="linux"
-if [ -n "$(uname | grep "Darwin")" ]; then
+if uname | grep -q "Darwin"; then
     PLATFORM="darwin"
 fi
-RELEASE_TAG="<< parameters.release-tag >>"
-#RELEASE_URL="https://api.github.com/repos/kubernetes-sigs/aws-iam-authenticator/releases/latest"
+export RELEASE_TAG=${!PARAM_RELEASE_TAG}
 FILENAME="aws-iam-authenticator"
 VERSION=$(curl -Ls --fail --retry 3 -o /dev/null -w "%{url_effective}" "https://github.com/kubernetes-sigs/aws-iam-authenticator/releases/latest" | sed 's:.*/::' | sed 's/v//g')
 if [ -n "${RELEASE_TAG}" ]; then
@@ -17,13 +16,12 @@ if [ -n "${RELEASE_TAG}" ]; then
     FILENAME="heptio-authenticator-aws"
     fi
 fi
-# extract version number
-VERSION_NUMBER=$(echo "$VERSION" | cut -c 2-)
-DOWNLOAD_URL="https://github.com/kubernetes-sigs/aws-iam-authenticator/releases/download/${VERSION}/${FILENAME}_${VERSION_NUMBER}_${PLATFORM}_amd64"
-$SUDO echo "HERE IS THE DOWNLOAD URL:" "$DOWNLOAD_URL"
+
+DOWNLOAD_URL="https://github.com/kubernetes-sigs/aws-iam-authenticator/releases/download/v${VERSION}/${FILENAME}_${VERSION}_${PLATFORM}_amd64"
+
 curl -L --fail --retry 3 -o aws-iam-authenticator "$DOWNLOAD_URL"
 chmod +x ./aws-iam-authenticator
-SUDO=""
+
 if [ "$(id -u)" -ne 0 ] && which sudo > /dev/null ; then
     SUDO="sudo"
 fi
