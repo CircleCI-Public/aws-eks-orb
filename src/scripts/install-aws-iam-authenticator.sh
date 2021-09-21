79f6297bb533381c692f86bf02cc9f4197d4b0f3
Brian Vu <emailbrian.vu@gmail.com>
2021-09-20 22:16:45 -0700

    chore: debugging ci issues

--- src/scripts/install-aws-iam-authenticator.sh
+++ src/scripts/install-aws-iam-authenticator.sh
@@ -1,32 +1,30 @@
 #!/bin/bash
-# if which aws-iam-authenticator > /dev/null; then
-#     echo "AWS IAM Authenticator for Kubernetes is already installed"
-#     exit 0
-# fi
+if which aws-iam-authenticator > /dev/null; then
+    echo "AWS IAM Authenticator for Kubernetes is already installed"
+    exit 0
+fi
 PLATFORM="linux"
 # if [ "$(uname | grep -q "Darwin")" ]; then
 if uname | grep -q 'Darwin' 
-then
+    then
     PLATFORM="darwin"
 fi
-echo $PLATFORM
+export RELEASE_TAG=${!PARAM_RELEASE_TAG}
+FILENAME="aws-iam-authenticator"
+VERSION=$(curl -Ls --fail --retry 3 -o /dev/null -w "%{url_effective}" "https://github.com/kubernetes-sigs/aws-iam-authenticator/releases/latest" | sed 's:.*/::' | sed 's/v//g')
+if [ -n "${RELEASE_TAG}" ]; then
+    VERSION="${RELEASE_TAG}"
+    if [ "${VERSION}" == "0.3.0" ]; then
+    FILENAME="heptio-authenticator-aws"
+    fi
+fi
 
-# export RELEASE_TAG=${!PARAM_RELEASE_TAG}
-# FILENAME="aws-iam-authenticator"
-# VERSION=$(curl -Ls --fail --retry 3 -o /dev/null -w "%{url_effective}" "https://github.com/kubernetes-sigs/aws-iam-authenticator/releases/latest" | sed 's:.*/::' | sed 's/v//g')
-# if [ -n "${RELEASE_TAG}" ]; then
-#     VERSION="${RELEASE_TAG}"
-#     if [ "${VERSION}" == "0.3.0" ]; then
-#     FILENAME="heptio-authenticator-aws"
-#     fi
-# fi
+DOWNLOAD_URL="https://github.com/kubernetes-sigs/aws-iam-authenticator/releases/download/v${VERSION}/${FILENAME}_${VERSION}_${PLATFORM}_amd64"
 
-# DOWNLOAD_URL="https://github.com/kubernetes-sigs/aws-iam-authenticator/releases/download/v${VERSION}/${FILENAME}_${VERSION}_${PLATFORM}_amd64"
+curl -L --fail --retry 3 -o aws-iam-authenticator "$DOWNLOAD_URL"
+chmod +x ./aws-iam-authenticator
 
-# curl -L --fail --retry 3 -o aws-iam-authenticator "$DOWNLOAD_URL"
-# chmod +x ./aws-iam-authenticator
-
-# if [ "$(id -u)" -ne 0 ] && which sudo > /dev/null ; then
-#     SUDO="sudo"
-# fi
-# $SUDO mv ./aws-iam-authenticator /usr/local/bin/aws-iam-authenticator
+if [ "$(id -u)" -ne 0 ] && which sudo > /dev/null ; then
+    SUDO="sudo"
+fi
+$SUDO mv ./aws-iam-authenticator /usr/local/bin/aws-iam-authenticator
