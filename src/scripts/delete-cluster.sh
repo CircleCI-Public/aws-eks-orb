5bc911ed4cd8b32479491904d17dc40a8255f326
Brian Vu <emailbrian.vu@gmail.com>
2021-11-08 09:12:55 -0800

    fix: adding commands before testing

---
+++ src/scripts/delete-cluster.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+CLUSTER_NAME=$(eval echo "$PARAM_CLUSTER_NAME")
+CONFIG_FILE=$(eval echo "$PARAM_CONFIG_FILE")
+AWS_REGION=$(eval echo "$PARAM_AWS_REGION")
+AWS_PROFILE=$(eval echo "$PARAM_AWS_PROFILE")
+WAIT=$(eval echo "$PARAM_WAIT")
+CFN_ROLE_ARN=$(eval echo "$PARAM_CFN_ROLE_ARN")
+VERBOSE=$(eval echo "$PARAM_VERBOSE")
+AWS_MAX_POLLING_WAIT_TIME=$(eval echo "$PARAM_AWS_MAX_POLLING_WAIT_TIME")
+
+if [ -n "${CLUSTER_NAME}" ]; then
+    set -- "$@" --name="${CLUSTER_NAME}"
+fi
+if [ -n "${CONFIG_FILE}" ]; then
+    set -- "$@" --config-file="${CONFIG_FILE}"
+fi
+if [ -n "${AWS_REGION}" ]; then
+    set -- "$@" --region="${AWS_REGION}"
+fi
+if [ -n "${AWS_PROFILE}" ]; then
+    set -- "$@" --profile="${AWS_PROFILE}"
+fi
+if [ "${WAIT}" == "true" ]; then
+    set -- "$@" --wait
+fi
+if [ -n "${CFN_ROLE_ARN}" ]; then
+    set -- "$@" --cfn-role-arn="${CFN_ROLE_ARN}"
+fi
+if [ -n "${AWS_MAX_POLLING_WAIT_TIME}" ]; then
+    set -- "$@" --timeout="${AWS_MAX_POLLING_WAIT_TIME}"
+fi
+set -- "$@" --verbose="${VERBOSE}"
+
+if [ "$SHOW_EKSCTL_COMMAND" == "1" ]; then
+    set -x
+fi
+
+eksctl delete cluster "$@"
+
+if [ "$SHOW_EKSCTL_COMMAND" == "1" ]; then
+    set +x
+fi
---
+++ src/scripts/update-kubeconfig-with-authenticator.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+CLUSTER_NAME=$(eval echo "$PARAM_CLUSTER_NAME")
+AWS_REGION=$(eval echo "$PARAM_AWS_REGION")
+AWS_PROFILE=$(eval echo "$PARAM_AWS_PROFILE")
+KUBECONFIG_FILE_PATH=$(eval echo "$PARAM_KUBECONFIG_FILE_PATH")
+ROLE_ARN=$(eval echo "PARAM_ROLE_ARN")
+PARAM_CLUSTER_CONTEXT_ALIAS=$(eval echo "$PARAM_CLUSTER_CONTEXT_ALIAS")
+PARAM_DRY_RUN=$(eval echo "$PARAM_DRY_RUN")
+VERBOSE=$(eval echo "$PARAM_VERBOSE")
+
+if [ -n "${CLUSTER_NAME}" ]; then
+    set -- "$@" --name "${CLUSTER_NAME}"
+fi
+if [ -n "${AWS_REGION}" ]; then
+    set -- "$@" --region "${AWS_REGION}"
+fi
+if [ -n "${AWS_PROFILE}" ]; then
+    set -- "$@" --profile "${AWS_PROFILE}"
+fi
+if [ -n "${KUBECONFIG_FILE_PATH}" ]; then
+    set -- "$@" --kubeconfig "${KUBECONFIG_FILE_PATH}"
+fi
+if [ -n "${ROLE_ARN}" ]; then
+    set -- "$@" --role-arn "${ROLE_ARN}"
+fi
+if [ -n "${CLUSTER_CONTEXT_ALIAS}" ]; then
+    set -- "$@" --alias "${CLUSTER_CONTEXT_ALIAS}"
+fi
+if [ "${DRY_RUN}" == "true" ]; then
+    set -- "$@" --dry-run
+fi
+if [ "${VERBOSE}" == "true" ]; then
+    set -- "$@" --verbose
+fi
+aws eks update-kubeconfig "$@"
