27514a6baca5690eced4fd7c076ec122d0286f72
Brian Vu <emailbrian.vu@gmail.com>
2023-05-22 12:42:19 -0700

    fix: edit link to include platform

--- src/scripts/setup.sh
+++ src/scripts/setup.sh
@@ -4,11 +4,11 @@ if [[ $EUID == 0 ]]; then export SUDO=""; else export SUDO="sudo"; fi
 
 # Define current platform
 if [[ "$(uname -s)" == "Darwin" && "$(uname -m)" == "x86_64" ]]; then
-	export SYS_ENV_PLATFORM=macos
+	export SYS_ENV_PLATFORM=Darwin_amd64
 elif [[ "$(uname -s)" == "Linux" && "$(uname -m)" == "x86_64" ]]; then
-	export SYS_ENV_PLATFORM=linux_x86
+	export SYS_ENV_PLATFORM=Linux_amd64
 elif [[ "$(uname -s)" == "Linux" && "$(uname -m)" == "aarch64" ]]; then
-	export SYS_ENV_PLATFORM=linux_arm
+	export SYS_ENV_PLATFORM=linux_arm64
 else
 	echo "This platform appears to be unsupported."
 	uname -a
@@ -17,7 +17,7 @@ fi
 
 if ! command -v eksctl >/dev/null 2>&1; then
 	echo "Installing the AWS eksctl CLI"
-    curl --silent --location "https://github.com/weaveworks/eksctl/releases/latest/download/eksctl_$(uname -s)_$(uname -m).tar.gz" | tar xz -C /tmp
+    curl --silent --location "https://github.com/weaveworks/eksctl/releases/latest/download/eksctl_${SYS_ENV_PLATFORM}.tar.gz" | tar xz -C /tmp
     $SUDO mv /tmp/eksctl /usr/local/bin
 	# Validate install.
 	echo
