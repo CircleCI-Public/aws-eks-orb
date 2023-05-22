22cf3d4cec06d86c515836fd0f4f2a7854199673
Brian Vu <emailbrian.vu@gmail.com>
2023-05-22 12:11:11 -0700

    refactor: edit variable names

--- src/scripts/update-kubeconfig-with-authenticator.sh
+++ src/scripts/update-kubeconfig-with-authenticator.sh
@@ -1,35 +1,34 @@
 #!/bin/bash
-CLUSTER_NAME=$(eval echo "$PARAM_CLUSTER_NAME")
-AWS_REGION=$(eval echo "$PARAM_AWS_REGION")
-AWS_PROFILE=$(eval echo "$PARAM_AWS_PROFILE")
-KUBECONFIG_FILE_PATH=$(eval echo "$PARAM_KUBECONFIG_FILE_PATH")
-ROLE_ARN=$(eval echo "$PARAM_ROLE_ARN")
-CLUSTER_CONTEXT_ALIAS=$(eval echo "$PARAM_CLUSTER_CONTEXT_ALIAS")
-DRY_RUN=$(eval echo "$PARAM_DRY_RUN")
-VERBOSE=$(eval echo "$PARAM_VERBOSE")
+ORB_EVAL_CLUSTER_NAME=$(eval echo "$ORB_EVAL_CLUSTER_NAME")
+ORB_EVAL_AWS_REGION=$(eval echo "$ORB_EVAL_AWS_REGION")
+ORB_EVAL_AWS_PROFILE=$(eval echo "$ORB_EVAL_AWS_PROFILE")
+ORB_EVAL_KUBECONFIG_FILE_PATH=$(eval echo "$ORB_EVAL_KUBECONFIG_FILE_PATH")
+ORB_EVAL_ROLE_ARN=$(eval echo "$ORB_EVAL_ROLE_ARN")
+ORB_EVAL_CLUSTER_CONTEXT_ALIAS=$(eval echo "$ORB_EVAL_CLUSTER_CONTEXT_ALIAS")
+ORB_EVAL_DRY_RUN=$(eval echo "$ORB_EVAL_DRY_RUN")
 
-if [ -n "${CLUSTER_NAME}" ]; then
-    set -- "$@" --name "${CLUSTER_NAME}"
+if [ -n "${ORB_EVAL_CLUSTER_NAME}" ]; then
+    set -- "$@" --name "${ORB_EVAL_CLUSTER_NAME}"
 fi
-if [ -n "${AWS_REGION}" ]; then
-    set -- "$@" --region "${AWS_REGION}"
+if [ -n "${ORB_EVAL_AWS_REGION}" ]; then
+    set -- "$@" --region "${ORB_EVAL_AWS_REGION}"
 fi
-if [ -n "${AWS_PROFILE}" ]; then
-    set -- "$@" --profile "${AWS_PROFILE}"
+if [ -n "${ORB_EVAL_AWS_PROFILE}" ]; then
+    set -- "$@" --profile "${ORB_EVAL_AWS_PROFILE}"
 fi
-if [ -n "${KUBECONFIG_FILE_PATH}" ]; then
-    set -- "$@" --kubeconfig "${KUBECONFIG_FILE_PATH}"
+if [ -n "${ORB_EVAL_KUBECONFIG_FILE_PATH}" ]; then
+    set -- "$@" --kubeconfig "${ORB_EVAL_KUBECONFIG_FILE_PATH}"
 fi
-if [ -n "${ROLE_ARN}" ]; then
-    set -- "$@" --role-arn "${ROLE_ARN}"
+if [ -n "${ORB_EVAL_ROLE_ARN}" ]; then
+    set -- "$@" --role-arn "${ORB_EVAL_ROLE_ARN}"
 fi
-if [ -n "${CLUSTER_CONTEXT_ALIAS}" ]; then
-    set -- "$@" --alias "${CLUSTER_CONTEXT_ALIAS}"
+if [ -n "${ORB_EVAL_CLUSTER_CONTEXT_ALIAS}" ]; then
+    set -- "$@" --alias "${ORB_EVAL_CLUSTER_CONTEXT_ALIAS}"
 fi
-if [ -n "${DRY_RUN}" ]; then
+if [ -n "${ORB_EVAL_DRY_RUN}" ]; then
     set -- "$@" --dry-run "${DRY_RUN}"
 fi
-if [ "${VERBOSE}" == "true" ]; then
+if [ "${ORB_VAL_VERBOSE}" == "true" ]; then
     set -- "$@" --verbose
 fi
 
