8d982b09d3be4964a031339275c452461115078d
Brian Vu <emailbrian.vu@gmail.com>
2024-01-11 23:25:08 -0800

    refactor: used utils in setup.sh

--- src/scripts/install_aws_iam_authenticator.sh
+++ src/scripts/install_aws_iam_authenticator.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 set -x
-ORB_VAL_RELEASE_TAG="$(echo "${ORB_VAL_RELEASE_TAG}" |  circleci env subst)"
+AWS_EKS_STR_RELEASE_TAG="$(echo "${AWS_EKS_STR_RELEASE_TAG}" |  circleci env subst)"
 
 if which aws-iam-authenticator > /dev/null; then
     echo "AWS IAM Authenticator for Kubernetes is already installed"
@@ -12,25 +12,10 @@ detect_os
 detect_arch
 set_sudo
 
-
-# PLATFORM="linux"
-# if uname | grep -q 'Darwin' 
-# then
-#     PLATFORM="darwin"
-# fi
-
-
 FILENAME="aws-iam-authenticator"
-# VERSION=0.5.6
-# if [ -n "${ORB_VAL_RELEASE_TAG}" ]; then
-#     export RELEASE_TAG=${ORB_VAL_RELEASE_TAG}
-#     VERSION="${RELEASE_TAG}"
-#     if [ "${VERSION}" == "0.3.0" ]; then
-#     FILENAME="heptio-authenticator-aws"
-#     fi
-# fi
-if [ "${ORB_VAL_RELEASE_TAG}" != "latest" ]; then
-    VERSION="${ORB_VAL_RELEASE_TAG}"
+
+if [ -n "${AWS_EKS_STR_RELEASE_TAG}" ]; then
+    VERSION="${AWS_EKS_STR_RELEASE_TAG}"
     if [ "${VERSION}" == "0.3.0" ]; then
         FILENAME="heptio-authenticator-aws"
     fi
@@ -45,8 +30,5 @@ printf "Downloading version %s\n"  "${VERSION}"
 curl -L --fail --retry 3 -o aws-iam-authenticator "$DOWNLOAD_URL"
 chmod +x ./aws-iam-authenticator
 
-# if [ "$(id -u)" -ne 0 ] && which sudo > /dev/null ; then
-#     SUDO="sudo"
-# fi
 $SUDO mv ./aws-iam-authenticator /usr/local/bin/aws-iam-authenticator
 set +x
